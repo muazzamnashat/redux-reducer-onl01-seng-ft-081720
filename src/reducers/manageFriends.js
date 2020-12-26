75beb2e61fdf21c0e7e4ae0ce41122011ac522fd
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-12-26 07:54:11 -0500

    commit tree f79ea87eaef4c6a3923a1be264506e52a8aa780e

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,7 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends:[]}, action){
+    if (action.type === "ADD_FRIEND") {
+        return {friends: [...state.friends,action.friend]}        
+    } else if (action.type === "REMOVE_FRIEND") {
+        return {friends : state.friends.filter(f => f.id !== action.id)}
+    } else return state
 }
